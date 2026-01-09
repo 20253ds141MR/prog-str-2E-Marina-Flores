3f3538cdc0be8282f35e0ad4ddc8524154de8caa
Tu Nombre Apellido <tu-correo@ejemplo.com>
2026-01-09 13:29:39 -0600

    Proyecto 01: Hola Mundo

---
+++ proyecto-01-hola-mundo/Main1.java
@@ -0,0 +1,8 @@
+public class Main1 {
+   public static void main(String[] args) {
+    System.out.println("Nombre: Marina Flores");
+    System.out.println("Grupo: 2E");
+    System.out.println("Fecha: 08/01/2026");
+    System.out.println("Hola mundo");
+   }
+}
