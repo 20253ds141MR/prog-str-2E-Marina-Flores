6d9786f4dbfa8ba2cad34d00d3921ced2e5cd691
Tu Nombre Apellido <tu-correo@ejemplo.com>
2026-01-17 23:03:01 -0600

    agregar calculo de iva

--- GitConflictoFloresMarina/Main.java
+++ GitConflictoFloresMarina/Main.java
@@ -7,6 +7,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        double IVA= 0.16;
         double subtotal = pedirDouble(scanner, "Subtotal: ");
         double total = subtotal;
 
