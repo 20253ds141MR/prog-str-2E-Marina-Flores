89c76012ed57294f10057f5e81d719f058a8f4c4
Tu Nombre Apellido <tu-correo@ejemplo.com>
2026-01-18 00:28:45 -0600

    refactor y buenas prácticas

--- ActividadRefactorFloresMarina/Main.java
+++ ActividadRefactorFloresMarina/Main.java
@@ -8,12 +8,16 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        /*Solictar los números a sumar */
+        System.out.println("Ingrese los números de las incognitas a sumar:");
         int a = pedirEntero(scanner, "a: ");
         int b = pedirEntero(scanner, "b: ");
         int c = pedirEntero(scanner, "c: ");
 
+        /*Operacion con las incognitas introducidas por el usuario */
         int suma = a + b + c;
 
+        /*Proceso suma y promedio */
         System.out.println("suma=" + suma);
         System.out.println("prom=" + (suma / 3.0));
     }
